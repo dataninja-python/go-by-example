1a868da1da6a68c18c21f644ebcd51f88fe902f7
dataninja-python <aj@redstonema.com>
2023-05-23 20:46:39 -0400

    up to struct embedding

---
+++ structEmbedding/embed.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
+func main() {
+	co := container{base: base{num: 1},
+		str: "str1",
+	}
+	fmt.Printf("co={num: %v, str: %v}\n}", co.num, co.str)
+
+	fmt.Printf("also num: ", co.base.num)
+
+	fmt.Printf("describe: ", co.describe())
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co
+	fmt.Println("describer: ", d.describe())
+}
